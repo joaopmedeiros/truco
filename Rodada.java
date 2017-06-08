72efde3daec9987efd4c49ab58d1fe20ebdb7924
Joao Cecilio <joaop.medeiros@gmail.com>
2017-06-08 08:45:08 -0300

    Initial commit

---
+++ Rodada.java
@@ -0,0 +1,15 @@
+/**
+ * Classe que controla os pontos da rodada
+ *
+ */
+public class Rodada {
+    /**
+     * ganhador - Usamos 0 para maquina, 1 para jogador.
+     */
+    private int pontosValendo;
+    private int ganhador;
+
+
+
+
+}
